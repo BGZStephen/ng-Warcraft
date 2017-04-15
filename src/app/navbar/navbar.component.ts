084fb61b623aa2ad8b6758e55ae99f7eaa7090ff
Stephen Wright <sjw948@gmail.com>
2017-04-15 18:00:55 +0100

    built responsive navbar and added logo

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,6 +9,34 @@ export class NavbarComponent implements OnInit {
 
   constructor() { }
 
+  navbarVisible = true;
+
+  navbarToggleResize() {
+    if (screen.width < 768) {
+      this.navbarToggle("resize")
+    }
+  }
+
+  navbarClose() {
+    if (screen.width < 768) {
+      console.log("Closing")
+      this.navbarVisible = true;
+      this.navbarToggle();
+    }
+  }
+
+  navbarToggle(resize?) {
+    if (resize && screen.width < 768) {
+      return {'visibility':'hidden', 'opacity': '0'}
+    } else if (this.navbarVisible == true && screen.width >= 768) {
+      return {'visibility':'visible', 'opacity': '1'}
+    } else if (this.navbarVisible == false) {
+      return {'visibility':'visible', 'opacity': '1'}
+    } else {
+      return {'visibility':'hidden', 'opacity': '0'}
+    }
+  }
+
   ngOnInit() {
   }
 
