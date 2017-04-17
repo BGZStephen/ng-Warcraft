8aded249214d81fcf40052e17020c3fab8a2c09a
Stephen Wright <sjw948@gmail.com>
2017-04-17 16:44:02 +0100

    pets master list call + styling finished

--- src/app/pages/api/api-pet/api-pet.component.ts
+++ src/app/pages/api/api-pet/api-pet.component.ts
@@ -10,33 +10,33 @@ export class ApiPetComponent implements OnInit {
 
   constructor(private apiService: WowApiService) { }
 
-  petResult: object;
-  petAbilityResult: object;
-  petSpeciesResult: object;
-  petStatsResult: object;
+  petsResults: object;
+  petAbilityResults: object;
+  petSpeciesResults: object;
+  petStatsResults: object;
 
   clearResults() {
-    this.petResult = null,
-    this.petAbilityResult = null,
-    this.petSpeciesResult = null,
-    this.petStatsResult = null
+    this.petsResults = null,
+    this.petAbilityResults = null,
+    this.petSpeciesResults = null,
+    this.petStatsResults = null
   }
 
-  searchPet() {
+  searchPets() {
     this.clearResults()
     this.apiService.apiSearch("Pet")
     .subscribe(res => {
-      this.petResult = res
-      console.log(this.petResult)
+      this.petsResults = res
+      console.log(this.petsResults)
     })
   }
 
-  searchPetAbilities(id) {
+  searchPetAbility(id) {
     this.clearResults()
     this.apiService.apiSearch("PetAbility", id)
     .subscribe(res => {
-      this.petAbilityResult = res
-      console.log(this.petAbilityResult)
+      this.petAbilityResults = res
+      console.log(this.petAbilityResults)
     })
   }
 
@@ -44,8 +44,8 @@ export class ApiPetComponent implements OnInit {
     this.clearResults()
     this.apiService.apiSearch("PetSpecies", id)
     .subscribe(res => {
-      this.petSpeciesResult = res
-      console.log(this.petSpeciesResult)
+      this.petSpeciesResults = res
+      console.log(this.petSpeciesResults)
     })
   }
 
@@ -53,8 +53,8 @@ export class ApiPetComponent implements OnInit {
     this.clearResults()
     this.apiService.apiSearch("PetStats", id)
     .subscribe(res => {
-      this.petStatsResult = res
-      console.log(this.petStatsResult)
+      this.petStatsResults = res
+      console.log(this.petStatsResults)
     })
   }
 
